f4c661ebc14870c5130b845b900a49537076afed
Huang Bo <huangbop@gmail.com>
2015-04-01 15:46:40 +0800

    **** defaultdict(<class 'list'>, {'A': ['server/images/kabuqinuo.jpg', 'server/images/meishikafei.jpg', 'server/products/kabuqinuo.json', 'server/products/meishikafei.json']})

--- src/rest.py
+++ src/rest.py
@@ -1,3 +1,5 @@
+"""*
+"""
 import requests
 import json
 
@@ -7,33 +9,46 @@ JSON_HEADERS = {
     "Content-Type": "application/json",
 }
 
-IMAGE_HEADERS = {
+JPG_HEADERS = {
     "X-Bmob-Application-Id": "e9c72808f8555c8d7846a13a50e907a6",
     "X-Bmob-REST-API-Key": "9632a84d440c4faa818f033355cb9bf3",
     "Content-Type": "image/jpeg",
 }
 
+PNG_HEADERS = {
+    "X-Bmob-Application-Id": "e9c72808f8555c8d7846a13a50e907a6",
+    "X-Bmob-REST-API-Key": "9632a84d440c4faa818f033355cb9bf3",
+    "Content-Type": "image/png",
+}
+
+
+
 CLASSES_BASE_URL = 'https://api.bmob.cn/1/classes/'
 FILES_BASE_URL = 'https://api.bmob.cn/1/files/'
 
 
-def add_record_image(info):
+def add_image(info):
+    """*
+    """
     import pdb; pdb.set_trace()
-    image = info['image']
-    res = requests.post(FILES_BASE_URL + image, headers=IMAGE_HEADERS,
-                        data=open('../server/product/images/' + image, 'rb'))
+    image_headers = JPG_HEADERS
+    if info['type'] == 'png':
+        image_headers = PNG_HEADERS
+    res = requests.post(FILES_BASE_URL + info['name'], headers=image_headers,
+                        data=info['bin'])
     if res.status_code == 201:
         file_desc = json.loads(res.content.decode())
         file_desc['__type'] = "File"
         info['file'] = file_desc
+        del info['bin']
+        res = requests.post('%s%s' % (CLASSES_BASE_URL, 'images'), headers=JSON_HEADERS,
+                            data=json.dumps(info))
+        print(res)
 
 
-def add_record(table, info):
-    import pdb; pdb.set_trace()
-    image = info.get('image')
-    if image:
-        add_record_image(info)
-
-    res = requests.post('%s%s' % (CLASSES_BASE_URL, table), headers=JSON_HEADERS,
+def add_product(info):
+    """*
+    """
+    res = requests.post('%s%s' % (CLASSES_BASE_URL, 'products'), headers=JSON_HEADERS,
                         data=json.dumps(info))
     print(res)
--- src/syn2db.py
+++ src/syn2db.py
@@ -1,3 +1,5 @@
+"""*
+"""
 import subprocess as sp
 import collections
 
@@ -6,9 +8,12 @@ import json
 
 
 def product_checkout_useful_string():
-    sp.call('git add ../server/product')
-    products = sp.getoutput("git checkout |grep 'product/.*.json'").encode('gbk').decode()
+    import pdb; pdb.set_trace()
+    sp.call('git add ../server/')
+    useful_string = sp.getoutput("git checkout").encode('utf16')
+    # |grep 'server/.*.[jpg|png|json]'")
     return useful_string
+
 
 def parse_useful_string():
     """Return a changed defaultdict
@@ -22,6 +27,7 @@ def parse_useful_string():
         changed_json_files[pairs[0]].append(pairs[1])
     return changed_json_files
 
+
 def dispatch_action():
     changed_json_files = parse_useful_string()
     adds = changed_json_files.get('A')
@@ -31,16 +37,42 @@ def dispatch_action():
     if not changed_json_files == {}:
         do_commit(changed_json_files)
 
+
+def add_image(add_file):
+    """*
+    """
+    file_name = add_file[add_file.rindex('/') + 1:]
+    file_type = add_file[add_file.rindex('.') + 1:]
+    file_bin = open('../server/images/' + file_name, 'rb')
+    info = {'name': file_name, 'type': file_type, 'bin': file_bin}
+    rest.add_image(info)
+
+
+def add_json(add_file):
+    """*
+    """
+    file_name = add_file[add_file.rindex('/') + 1: -5]
+    info = json.load(open('../products/%s' % add_file))
+    info['name'] = file_name
+    rest.add_product(info)
+
+
 def do_adds(adds):
+    """*
+    """
     for add_file in adds:
         import pdb; pdb.set_trace()
-        file_name = add_file[add_file.rindex('/') + 1 : -5]
-        info = json.load(open('../%s' % add_file))
-        info['name'] = file_name
-        rest.add_record('product', info)
+        if add_file.find('images') != -1:
+            add_image(add_file)
+        elif add_file.find('products') != -1:
+            add_json(add_file)
+
 
 def do_commit(files):
-    sp.call('git commit -m "******** %s"' % str(files))
+    """*
+    """
+    # sp.call('git commit -m "******** %s"' % str(files))
+    pass
 
 
 if __name__ == '__main__':
